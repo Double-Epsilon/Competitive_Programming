6385e0a45118205a283abb3d40f44dbf863825a1
Double-Epsilon <er2352387@gmail.com>
2025-05-26 13:47:59 -0600

    Added Data_Types.cpp

---
+++ Basics (In-Out-Conditionals)/Data_Types.cpp
@@ -0,0 +1,14 @@
+#include <bits/stdc++.h> // Libreria <bits/stdc++.h>
+using namespace std; // Evitar colocar std::
+
+int main(){
+    cin.tie(0) -> sync_with_stdio(); cout.tie(0); // Optimizacion de entrada/salida
+    int entero = 123; // Enteros desde -(2^32) hasta 2^32 (2^32 = 10^9 aprox)
+    long long enteroGrande = 1234567890; // Enteros desde -(2^64) hasta 2^64 (2^64 = 10^18 aprox)
+    float decimal = 123.456; // Decimales desde 1.2E-38 hasta 3.4E+38
+    double decimalGrande = 123.456789; // Decimales desde 2.3E-308 hasta 1.7E+308
+    bool booleano = true; // True o False (1 y 0)
+    char caracter = 'a'; // Caracteres ASCII (0 a 255)
+
+    return 0;
+}
